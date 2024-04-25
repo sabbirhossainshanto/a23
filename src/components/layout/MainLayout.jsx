cfa970993396708368617148aa4c60ac2617e505
sabbirHossain <sabbirshnt@gmail.com>
2024-04-25 20:57:47 +0600

    iframe

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../ui/Footer/Footer";
 import Header from "../ui/Header/Header";
 
 const MainLayout = () => {
+  const location = useLocation();
   return (
-    <div className="centered-div">
+    <div
+      className="centered-div"
+      style={{
+        paddingBottom: `${location.pathname.includes("/aviator") ? "0px" : ""}`,
+      }}
+    >
       <Header />
       <div style={{ minHeight: "100vh" }}>
         <Outlet />
       </div>
-      <Footer />
+      {!location.pathname.includes("/aviator") && <Footer />}
     </div>
   );
 };
