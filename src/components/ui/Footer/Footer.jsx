86d90d2ff80ae18fcefc48248f26a047c51ca1a8
sabbirHossain <sabbirshnt@gmail.com>
2024-05-09 16:34:09 +0600

    remove dot

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -177,7 +177,7 @@ const Footer = () => {
           style={{ cursor: "pointer" }}
           className="tabbar-item"
         >
-          <div className="dont-badge"></div>
+          {/* <div className="dont-badge"></div> */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
