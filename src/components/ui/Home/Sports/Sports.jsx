3ff52acfea9a7b3ee32c2521f00beadd2d6bb1de
sabbirHossain <sabbirshnt@gmail.com>
2024-07-10 15:34:52 +0600

    updated

--- src/components/ui/Home/Sports/Sports.jsx
+++ src/components/ui/Home/Sports/Sports.jsx
@@ -1,6 +1,6 @@
 import Card from "../LiveSports/Card";
 
-const Sports = ({ sports,sportsType }) => {
+const Sports = ({ sports, sportsType }) => {
   /* filtered visible = true sports */
 
   const filterSports =
@@ -18,33 +18,47 @@ const Sports = ({ sports,sportsType }) => {
       </div>
     );
   }
+  console.log(sports);
 
   return (
-    <div style={{display:'flex',flexDirection:'column',gap:'15px'}}>
-      {sports && Object.values(sports).length > 0
-        ? filterSports
-            ?.sort((keyA, keyB) => {
-              return sports[keyA].sort - sports[keyB].sort;
-            })
-            ?.sort((keyA, keyB) => {
-              if (
-                sports[keyA].timeStatus === "Suspended" &&
-                sports[keyB].timeStatus !== "Suspended"
-              ) {
-                return 1;
-              }
-              if (
-                sports[keyA].timeStatus !== "Suspended" &&
-                sports[keyB].timeStatus === "Suspended"
-              ) {
-                return -1;
-              }
-              return 0;
-            })
-            .map((key, index) => {
-              return <Card key={index} keys={key} liveSports={sports} sportsType={sportsType} />;
-            })
-        : null}
+    <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+      {sports && Object.values(sports).length > 0 ? (
+        filterSports
+          ?.sort((keyA, keyB) => {
+            return sports[keyA].sort - sports[keyB].sort;
+          })
+          ?.sort((keyA, keyB) => {
+            if (
+              sports[keyA].timeStatus === "Suspended" &&
+              sports[keyB].timeStatus !== "Suspended"
+            ) {
+              return 1;
+            }
+            if (
+              sports[keyA].timeStatus !== "Suspended" &&
+              sports[keyB].timeStatus === "Suspended"
+            ) {
+              return -1;
+            }
+            return 0;
+          })
+          .map((key, index) => {
+            return (
+              <Card
+                key={index}
+                keys={key}
+                liveSports={sports}
+                sportsType={sportsType}
+              />
+            );
+          })
+      ) : (
+        <div className="profile-menu-box">
+          <div className="card-blank">
+            <span> No events available right now</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
